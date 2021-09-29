15e61072f270706eb44c821a82cdbb48e31e9ebd
sm921 <seimei.221.921@gmail.com>
2021-09-29 17:53:07 +0900

    feat: Add Gauss-Siedel and Jacobi methods

--- ts/helpers/math/solver.test.ts
+++ ts/helpers/math/solver.test.ts
@@ -1,6 +1,58 @@
+/// <reference path="matrix.test.ts" />
 /// <reference path="solver.ts" />
+
 namespace MATH {
   describe("solver", () => {
+    test("Cholesky", () => {
+      const A = new Matrix([1, -1, 2, -1, 5, -4, 2, -4, 6], 3, 3);
+      expect(Solver.cholesky(A, [17, 31, -5])).toEqual(
+        new Float32Array([51.5, 4.5, -15])
+      );
+    });
+
+    test("Gauss-Siedel", () => {
+      [undefined, 7].forEach((iterationCount) =>
+        expectElementsEqualTo(
+          Solver.gaussSiedel(
+            new Matrix([16, 3, 7, -11], 2, 2),
+            Vector.ones(2),
+            new Vector([11, 13]),
+            iterationCount
+          ).elements,
+          [0.8122, -0.665],
+          1
+        )
+      );
+      expectElementsEqualTo(
+        Solver.gaussSiedel(
+          new Matrix(
+            [10, -1, 2, 0, -1, 11, -1, 3, 2, -1, 10, -1, 0, 3, -1, 8],
+            4,
+            4
+          ),
+          Vector.zero(4),
+          new Vector([6, 25, -11, 15])
+        ).elements,
+        [1, 2, -1, 1],
+        1
+      );
+    });
+
+    test("Jacobi", () => {
+      [undefined, 25].forEach((iterationCount) =>
+        expectElementsEqualTo(
+          Solver.jacobi(
+            new Matrix([2, 1, 5, 7], 2, 2),
+            Vector.ones(2),
+            new Vector([11, 13]),
+            iterationCount
+          ).elements,
+          [7.111, -3.222],
+          1
+        )
+      );
+    });
+
     const testLuSolver = (A: Matrix, b: number[], x: number[]) =>
       test("LU", () => {
         expect(Solver.lu(A, b)).toEqual(new Float32Array(x));
@@ -15,12 +67,5 @@ namespace MATH {
       [0, 4, 17],
       [2, 0, -1]
     );
-
-    test("Cholesky", () => {
-      const A = new Matrix([1, -1, 2, -1, 5, -4, 2, -4, 6], 3, 3);
-      expect(Solver.cholesky(A, [17, 31, -5])).toEqual(
-        new Float32Array([51.5, 4.5, -15])
-      );
-    });
   });
 }
--- ts/helpers/math/solver.ts
+++ ts/helpers/math/solver.ts
@@ -1,10 +1,129 @@
-/// <reference path="matrix.ts" />
+/// <reference path="math.ts" />
 
 namespace MATH {
   /**
    * solve linear system Ax = b
    */
   export class Solver {
+    /**
+     * solve Ax = b using cholesky decomposition
+     *
+     * return null if cholesky decomposition does not exist
+     * @param AorL
+     * @param b
+     * @param skipsDecomposition set trure if AorL is L
+     * @returns
+     */
+    static cholesky(
+      AorL: Matrix,
+      b: Float32Array | number[],
+      skipsDecomposition?: boolean
+    ): Float32Array | null {
+      const L = skipsDecomposition ? AorL : cholesky(AorL);
+      if (L === null) return null;
+      /*
+          
+          Ax = b
+          A = L*L' then LL'x = b
+          substituting L'x with y, Ly = b and L'x = y
+          solve Ly = b by forward substitution and L'x = y in backward substitution
+            y(i) = (b(i) - Sigma_k(L(i,k)*y(k)) / L(i,i)  (0<=k<i)
+            x(i) = (y(i) - Sigma_k(L'(i,k)*x(k)) / L'(i,i)  (i<k<=n)
+          */
+      const y = new Float32Array(b.length);
+      for (let i = 0; i < y.length; i++) {
+        let sigma = 0;
+        for (let k = 0; k < i; k++) sigma += L._(i, k) * y[k];
+        y[i] = (b[i] - sigma) / L._(i, i);
+      }
+      const x = new Float32Array(b.length);
+      for (let i = x.length - 1; i >= 0; i--) {
+        let sigma = 0;
+        for (let k = x.length - 1; k > i; k--) sigma += L._(k, i) * x[k];
+        x[i] = (y[i] - sigma) / L._(i, i);
+      }
+      return x;
+    }
+
+    /**
+     * solve A x = b by Gauss-Seidel method
+     * ```
+     *  A = L + U
+     *    where L and U are lower and strictly upper triangular components of A
+     *  update x by L^-1 (b - Ux)
+     *    or in element-based, xi = 1/a_ii (b_i - Sigma_j(0 to j-2) a_ij * x_j - Sigma_j(i to n-1) a_ij * x_j)
+     *  until A x - b is nearly equal to zero
+     * ```
+     * @param A
+     * @param x initial guess (ones or zeros of more acurate x, whatever)
+     * @param b
+     * @param iterationCount
+     * @returns
+     */
+    static gaussSiedel(
+      A: Matrix,
+      x: Vector,
+      b: Vector,
+      iterationCount?: number,
+      tolerance = 1e-3
+    ): Vector {
+      let k = 0;
+      while (true) {
+        for (let i = 0; i < A.height; i++) {
+          let sigma = 0;
+          for (let j = 0; j < A.width; j++)
+            if (j !== i) sigma += A._(i, j) * x._(j);
+          x.set(i, (b._(i) - sigma) / A._(i, i));
+        }
+        k++;
+        if (iterationCount === undefined) {
+          if (x.multiplyMatrix(A).subtractNew(b).squaredNorm() < tolerance)
+            break;
+        } else if (k > iterationCount) break;
+      }
+      return x;
+    }
+
+    /**
+     * solve A x = b by Jacobi method
+     * ```
+     *  A = D + L + U
+     *  update x by D^-1 (b - (L+U)x )
+     *    or in element-based, 1/a_ii (bi - Sigma_j a_ij * x_j)
+     *  until Ax - b is nealy equal to zero
+     * ```
+     * @param A
+     * @param x initial guess (ones or zeros of more acurate x, whatever)
+     * @param b
+     * @param iterationCount
+     * @returns
+     */
+    static jacobi(
+      A: Matrix,
+      x: Vector,
+      b: Vector,
+      iterationCount?: number,
+      tolerance = 1e-3
+    ): Vector {
+      let k = 0;
+      while (true) {
+        const x_new = x.clone();
+        for (let i = 0; i < A.height; i++) {
+          let sigma = 0;
+          for (let j = 0; j < A.width; j++)
+            if (j !== i) sigma += A._(i, j) * x._(j);
+          x_new.set(i, (b._(i) - sigma) / A._(i, i));
+        }
+        x = x_new;
+        k++;
+        if (iterationCount === undefined) {
+          if (x.multiplyMatrix(A).subtractNew(b).squaredNorm() < tolerance)
+            break;
+        } else if (k > iterationCount) break;
+      }
+      return x;
+    }
+
     /**
      * sole Ax = b by LU decomposition
      * @param A
@@ -48,46 +167,6 @@ namespace MATH {
         let sigma = 0;
         for (let k = i + 1; k <= x.length - 1; k++) sigma += U._(i, k) * x[k];
         x[i] = (y[i] - sigma) / U._(i, i);
-      }
-      return x;
-    }
-
-    /**
-     * solve Ax = b using cholesky decomposition
-     *
-     * return null if cholesky decomposition does not exist
-     * @param AorL
-     * @param b
-     * @param skipsDecomposition set trure if AorL is L
-     * @returns
-     */
-    static cholesky(
-      AorL: Matrix,
-      b: Float32Array | number[],
-      skipsDecomposition?: boolean
-    ): Float32Array | null {
-      const L = skipsDecomposition ? AorL : cholesky(AorL);
-      if (L === null) return null;
-      /*
-      
-      Ax = b
-      A = L*L' then LL'x = b
-      substituting L'x with y, Ly = b and L'x = y
-      solve Ly = b by forward substitution and L'x = y in backward substitution
-        y(i) = (b(i) - Sigma_k(L(i,k)*y(k)) / L(i,i)  (0<=k<i)
-        x(i) = (y(i) - Sigma_k(L'(i,k)*x(k)) / L'(i,i)  (i<k<=n)
-      */
-      const y = new Float32Array(b.length);
-      for (let i = 0; i < y.length; i++) {
-        let sigma = 0;
-        for (let k = 0; k < i; k++) sigma += L._(i, k) * y[k];
-        y[i] = (b[i] - sigma) / L._(i, i);
-      }
-      const x = new Float32Array(b.length);
-      for (let i = x.length - 1; i >= 0; i--) {
-        let sigma = 0;
-        for (let k = x.length - 1; k > i; k--) sigma += L._(k, i) * x[k];
-        x[i] = (y[i] - sigma) / L._(i, i);
       }
       return x;
     }
